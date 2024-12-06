05caed026ecb0030c9cc3e8a4f6b86e477e8156d
Scolastica Nzyoki <solanzioki18@gmail.com>
2024-12-07 00:58:44 +0300

    filetr'

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers,name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+
+}
+
+function fuzzyMatch(drivers,partialName){
+    return drivers.filter(driver => driver.startsWith(partialName));
+
+
+
+}
+
+function matchName(drivers,name){
+    return drivers.filter(driver => driver.name === name);
+
+}
